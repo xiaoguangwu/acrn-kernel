8806b0142860dcfe3ee3c9414b5f5ee47160a2aa
Xiaoguang Wu <xiaoguang.wu@intel.com>
2020-04-21 04:28:12 +0800

    Revert "drm/i915/gvt: Support vgpu workload priority config"
    This reverts commit c29d7e1364283487d0b9d470c52000e25287abfe.

--- drivers/gpu/drm/i915/i915_params.h
+++ drivers/gpu/drm/i915/i915_params.h
@@ -81,9 +81,7 @@ struct drm_printer;
 	param(int, domain_scaler_owner, 0x11100) \
 	param(unsigned int, enable_pvmmio, \
 			PVMMIO_ELSP_SUBMIT | PVMMIO_PPGTT_UPDATE) \
-	param(bool, enable_gvt, false) \
-	param(int, gvt_workload_priority, 0)
-
+	param(bool, enable_gvt, false)
 
 #define MEMBER(T, member, ...) T member;
 struct i915_params {
