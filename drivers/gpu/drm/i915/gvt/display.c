d7ebb0b5ed3982b88162598454867e2fb98df85e
Xiaoguang Wu <xiaoguang.wu@intel.com>
2020-04-21 04:26:21 +0800

    Revert "drm/i915/gvt: enable local direct display for WaaG"
    This reverts commit be307b5bec530d22b8e43b3c52e4d2b3a67ee169.

--- drivers/gpu/drm/i915/gvt/display.c
+++ drivers/gpu/drm/i915/gvt/display.c
@@ -548,7 +548,7 @@ void intel_gvt_init_pipe_info(struct intel_gvt *gvt)
 	}
 }
 
-bool gvt_emulate_hdmi; /* default value: false */
+bool gvt_emulate_hdmi = true;
 
 int setup_virtual_monitors(struct intel_vgpu *vgpu)
 {
